f631ab6d3f913ac7d41778fa732ec534f31e6b00
naynara87 <naynara87@gmail.com>
2021-08-05 15:46:22 +0900

    퍼블리싱 수정20210805

--- admin/assets/js/pages/admin-script.js
+++ admin/assets/js/pages/admin-script.js
@@ -45,6 +45,44 @@ $(function () {
         });
     });
 });
+
+//chart1
+$(function () {
+    var options = {
+        series: [{
+            name: "접속자",
+            data: ['0', '1', '2', '0', '0', '0', '16', '5', '12', '7', '10', '11', '3', '4', '14', '6', '11', '2', '0', '1', '50', '60', '20', '5']
+        }],
+        chart: {
+            toolbar: {
+                show: false
+            },
+            height: 500,
+            type: 'line',
+            zoom: {
+                enabled: false
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'straight'
+        },
+        grid: {
+            row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+            },
+        },
+        xaxis: {
+            categories: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'],
+        }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#userChart_01"), options);
+    chart.render();
+});
 /******/ })()
 ;
 //# sourceMappingURL=admin-script.js.map
